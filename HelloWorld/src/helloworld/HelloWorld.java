3339e67c6afc5517b363067ef6046e9a0cf2cfdd
manthanbhalla <manthan.bhalla4@gmail.com>
2017-01-19 15:17:04 -0500

    first commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Mahesh mahesh(200330980)
+ */
+class HelloWorld
+{
+// This program prints a simple message
+public static void main (String[] args)
+{
+System.out.println("Hello world!");
+}
+}
